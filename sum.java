48e6e4947b5bc41b2d3f996d23e9a3fdf6a7801f
Sayan Biswas <techfriends1234.github@gmail.com>
2019-10-10 12:10:45 +0530

    debugging

--- sum.java
+++ sum.java
@@ -2,8 +2,6 @@
 import java.util.Scanner;
 public class sum 
 {
-    Scanner sc = new Scanner(System.in);
-    boolean q1;
     public static void sum()
     {
         int a,b,c;
@@ -14,21 +12,5 @@ public class sum
         b=sc.nextInt();
         c=a+b;
         System.out.println("Sum is "+c);
-    }
-    public boolean First()
-    {
-        for(int i=1;i<4;i++)
-        {
-            System.out.println("Try number: "+i+"\nEnter your first name: ");
-            String first=sc.next();
-            q1=(first!="SAYAN")?true:false;
-            if(q1==true)break;
-            if(i==3)
-            {
-                System.out.println("You have crossed the number of trials! Access denied! ");
-                System.exit(0);
-            }
-        }
-        return q1;
     }
 }
