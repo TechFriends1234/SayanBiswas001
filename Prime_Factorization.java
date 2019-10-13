ff64a73e648f1434122dc0e04054a7b95240f9f3
Sayan Biswas <techfriends1234.github@gmail.com>
2019-10-13 20:13:07 +0530

    Bugged

--- Prime_Factorization.java
+++ Prime_Factorization.java
@@ -2,7 +2,7 @@ public class Prime_Factorization
 {
     public static void pfact(int n)
     {
-        int i=0, k=1,c=0,count=1,j=0,l=0;
+        int i=0, k=1,c=0,count=1,j=0;
         
         while(k<=n)
            {
@@ -16,10 +16,11 @@ public class Prime_Factorization
                }
                if(i==k && n%i==0)
                {
-                   j++;l++;
+                   j++;
                }
                k++;
            }
+           System.out.println(j);//no of prime factors
           i=0; k=1;c=0;count=1;
         int a1[] = new int[j];
         if(n>0)
@@ -42,15 +43,22 @@ public class Prime_Factorization
                 }
                 k++;
             }
+            
+            
+            
+            
             for(int x=0;x<j;x++)
             {
                 for(int y=x+1;y<j;y++)
+                for(int z=x+1;z<y;z++)
+                if(a1[x]!=a1[z])
                 if(a1[x]==a1[y])
                 count++;
                 System.out.print(a1[x]+"^"+count+" x ");
                 count=1;
             }
             System.out.println("");
+            
         }
         else
         System.out.println("Prime factor of " + n + " are: 2 3 5 ...");
